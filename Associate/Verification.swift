a457a4b9733f013332b6b2e4beff09a0d4b66ce8
VarunSaini02 <varun.saini@gmail.com>
2020-05-07 12:23:19 -0500

    Update page properties to include optional image, verification, identifier

---
+++ Associate/Verification.swift
@@ -0,0 +1,18 @@
+//
+//  Verification.swift
+//  Associate
+//
+//  Created by 90304566 on 5/7/20.
+//  Copyright © 2020 Eden Prairie Schools. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class Verification {
+    var images: [Image]?
+    
+    init (images: [Image]?) {
+        self.images = images
+    }
+}
