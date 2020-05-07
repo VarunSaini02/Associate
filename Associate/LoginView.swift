6c16e5489d7e81e32132a06c23a40cc44e6b7e8e
Bela Konkoly <43889637+konkolyb@users.noreply.github.com>
2020-05-07 08:46:28 -0500

    Created login page file

---
+++ Associate/LoginView.swift
@@ -0,0 +1,34 @@
+//
+//  login.swift
+//  Associate
+//
+//  Created by 90302939 on 5/5/20.
+//  Copyright © 2020 Eden Prairie Schools. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationView{
+        VStack {
+            Text("Enter Username")
+            Text("Enter Password")
+            NavigationLink(destination:
+            ContentView()){
+                Text("Login")
+            }
+        }
+  
+        }
+    .navigationBarTitle("")
+    .navigationBarBackButtonHidden(true)
+    .navigationBarHidden(true)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
