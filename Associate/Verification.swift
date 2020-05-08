6a9408727500be2854ad31ff04fd3f05285d549b
VarunSaini02 <varun.saini@gmail.com>
2020-05-07 21:37:16 -0500

    Add Contact, which include Name and Phone structs. Also add typeRemoving functionality and stale Verification property nullification.

--- Associate/Verification.swift
+++ Associate/Verification.swift
@@ -11,15 +11,17 @@ import SwiftUI
 
 class Verification {
     enum VerificationType {
-        case direct // organizer verifies through their own account (most reliable)
-        case images // pictures of volunteer slips, signatures, or other forms of proof
         case contact // contact information (name, email, phone, etc.) for someone who can verify (last resort)
+        case images // pictures of volunteer slips, signatures, or other forms of proof
+        case direct // organizer verifies through their own account (most reliable)
     }
 
     var types = [VerificationType]()
     var images: [Image]?
+    var contacts: [Contact]?
     
-    init (images: [Image]? = nil, types: [VerificationType]) {
+    init (types: [VerificationType], images: [Image]? = nil, contacts: [Contact]? = nil) {
+        self.contacts = contacts
         self.images = images
         
         for type in types where !self.types.contains(type) {
@@ -28,6 +30,58 @@ class Verification {
     }
     
     func removeType(typesToRemove: [VerificationType]) {
-        self.types.removeAll { typesToRemove.contains($0) }
+        var toDisable = [VerificationType]()
+        //self.types.removeAll { typesToRemove.contains($0) }
+        
+        self.types.removeAll {
+            if typesToRemove.contains($0) {
+                toDisable.append($0)
+                return true
+            } else { return false }
+        }
+        
+        
+    }
+    
+    func nullifyStaleProperties(types: [VerificationType]) {
+        for type in types {
+            switch type {
+            case .contact:
+                self.contacts = nil
+            case .images:
+                self.images = nil
+            case .direct:
+                break
+            }
+        }
+    }
+    
+    
+}
+
+struct Contact {
+    var name: Name?
+    var email: String?
+    var phone: PhoneNumber?
+    
+    init(name: Name? = nil, email: String? = nil, phone: PhoneNumber? = nil) {
+        self.name = name
+        self.email = email
+        self.phone = phone
+    }
+}
+
+struct PhoneNumber {
+    var areaCode: Int
+    var prefix: Int
+    var lineNumber: Int
+    var unformatted: Int
+    
+    init(_ areaCode: Int, _ prefix: Int, _ lineNumber: Int) {
+        self.areaCode = areaCode
+        self.prefix = prefix
+        self.lineNumber = lineNumber
+        
+        unformatted = (areaCode * 10000000) + (prefix * 10000) + lineNumber
     }
 }
