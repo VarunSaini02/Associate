0c60a96826b9f4bac25ecf8502be99ecf8562afa
VarunSaini02 <varun.saini@gmail.com>
2020-05-15 12:45:02 -0500

    Default objects for Tuesday May 19

--- Associate/Profile.swift
+++ Associate/Profile.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2020 Eden Prairie Schools. All rights reserved.
 //
 //
-//import UIKit
-//
+
+import UIKit
+
 class Profile {
     var name: Name
     var email: String
@@ -17,6 +18,22 @@ class Profile {
         self.name = Name(firstName, lastName)
         self.email = email
         self.book = book
+    }
+    
+    //just for Tuesday, May 19
+    init() {
+        self.name = Name("Bela","Konkoly")
+        self.email = "iOSIsAwesome@gmail.com"
+        
+        let identifier = "Teaching Kids How to Swim"
+        let description = "I helped kids from the age of 5–10 years old learn how to swim through a program that the American Red Cross facilitated."
+        let startDate = Date()
+        let endDate = startDate.addingTimeInterval(7200)
+        let verify = Verification(types: [.contact], contacts: [Contact(name: Name("Varun","Saini"), email: "BlakeBranvold@gmail.com", phone: PhoneNumber(123, 456, 7890))])
+        
+        let pages = [Page(identifier: identifier, description: description, startDate: startDate, endDate: endDate, verification: verify)]
+        
+        self.book = Book(chapters: [Chapter(pages: pages, identifier: "Community Service")])
     }
 }
 
